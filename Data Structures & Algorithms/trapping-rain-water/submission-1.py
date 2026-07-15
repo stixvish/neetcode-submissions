690f44c14457f40c231e8c771510fad198328a26
Vishesh Gupta <gupta.v.vishesh@gmail.com>
2026-07-15 13:56:13 -0500

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        trapped = 0
+        for index, value in enumerate(height):
+            maxL = max(height[0:index]) if height[0:index] else 0
+            maxR = max(height[index+1:len(height)]) if height[index+1:len(height)] else 0
+            print(index, value, maxL, maxR, min(maxL, maxR) - value)
+            trapped += min(maxL, maxR) - value if min(maxL, maxR) - value >= 0 else 0
+        return trapped
